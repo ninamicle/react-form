e816968cdec987c78d2de2dc713214a9ea6f5063
NINA_MICLEUSANU <NINA.MICLEUSANU93@gmail.com>
2019-11-05 13:20:48 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      names: []
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    var name = this.refs.name.value;
+    console.log('hai scritto ' + name)
+    //pulisco il form
+    this.refs.name.value = '';
+    let names = this.state.names.slice();
+    names.push(name);
+    this.setState ({names: names});
+  }
+  render(){
+    let listaNomi = this.state.names.map((name, index) =>{
+return(
+  <li key={index}>{name}</li>
+)
+    });
+    return(
+      <div>
+        <h1>Foglio iscrizioni</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type='text' placeholder='Il tuo nome' ref='name'/>
+          <input type='submit' value='Invia'/>
+        </form>
+        <div>
+          <h2>Nomi gia iscritti:</h2>
+          <ol>{listaNomi}</ol>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
